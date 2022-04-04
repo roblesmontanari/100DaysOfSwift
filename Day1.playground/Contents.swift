58ecb45a1800bbe350f3a0a5e46dda51a374d434
Robles Montanari <robles.png@gmail.com>
2022-04-04 14:41:13 -0300

    First commit

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,90 @@
+// DAY 1: variables, constants, strings and numbers. //
+
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Robles"
+name = "Mariana"
+name = "Felipe"
+
+let character = "José"
+
+var playerName = "Roberto"
+print(playerName)
+
+playerName = "Robles"
+print(playerName)
+
+playerName = "Ricardo"
+print(playerName)
+
+let managerName = "Michel"
+let dogBreed = "Vira-lata Caramelo"
+let meaningOfLife = "How many roads must a man walk down?"
+
+//
+
+let actor = "Denzel Washington"
+let filename = "paris.jpg"
+let result = "You won!"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+//
+
+let score = 10
+let reallyBig = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 2
+let doubledScore = score * 2
+let squaredScore = score * score
+let halfedScore = score / 2
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+let number1 = 120
+print(number1.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
+
+//
+
+let number2 = 0.1 + 0.2
+print(number2)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name1 = "Nicolas Cage"
+
+var rating = 5.0
+rating += 2
+
+//
