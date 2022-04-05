f85fb1185df883d2a6b549a4931cb3b441e552cc
Robles Montanari <robles.png@gmail.com>
2022-04-05 12:17:58 -0300

    Day 3 commit

--- day1-14_Introduction/Day3.playground/Contents.swift
+++ day1-14_Introduction/Day3.playground/Contents.swift
@@ -100,4 +100,19 @@ print(actors)
 
 //
 
+/*
+var selected = "Monday"
+selected = "Tuesday"
+selected = "January"
+selected = "Friday "
+*/
 
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = Weekday.tuesday
+day = .friday
+
+//
