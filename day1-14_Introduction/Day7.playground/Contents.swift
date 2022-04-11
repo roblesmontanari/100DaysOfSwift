4848046b4c364b1b045679acac335c9e5f580969
Robles Montanari <robles.png@gmail.com>
2022-04-11 14:19:05 -0300

    Day 7 commit

--- day1-14_Introduction/Day7.playground/Contents.swift
+++ day1-14_Introduction/Day7.playground/Contents.swift
@@ -26,3 +26,30 @@ func printTimesTables(number: Int, end: Int) {
 }
 
 printTimesTables(number: 5, end: 20)
+
+//
+
+let root = sqrt(169)
+print(root)
+
+func rollDice() -> Int {
+    return Int.random(in: 1...6)
+}
+
+let result = rollDice()
+print(result)
+
+func areLettersIdentical(string1: String, string2: String) -> Bool {
+    return string1.sorted() == string2.sorted()
+}
+
+func pythagoras(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
+
+func sayHello() {
+    return
+}
