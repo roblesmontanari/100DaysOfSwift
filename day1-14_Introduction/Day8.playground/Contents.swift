11147489ac01c9d8392872c7270d9d17cf067f15
Robles Montanari <robles.png@gmail.com>
2022-04-12 15:51:07 -0300

    Day 8 commit

---
+++ day1-14_Introduction/Day8.playground/Contents.swift
@@ -0,0 +1,19 @@
+// DAY 8: default valueas, throwing functions, and checkpoint 4 //
+
+import Cocoa
+
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+characters.removeAll(keepingCapacity: true)
+print(characters.count)
+
+//
