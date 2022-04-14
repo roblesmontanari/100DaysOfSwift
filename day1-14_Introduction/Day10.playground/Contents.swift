3aa0208dbc462235310a87e67b4663e38b9d2989
Robles Montanari <robles.png@gmail.com>
2022-04-14 16:43:38 -0300

    Day 10 commit

--- day1-14_Introduction/Day10.playground/Contents.swift
+++ day1-14_Introduction/Day10.playground/Contents.swift
@@ -67,3 +67,37 @@ john.vacationRemaining1 = 5
 print(john.vacationAllocated)
 
 //
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts")
+            print("Old values was: \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+//
