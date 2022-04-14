a84b84315da393ea5a2085e847e0c61a92330ddf
Robles Montanari <robles.png@gmail.com>
2022-04-13 21:56:34 -0300

    Day 9 commit

--- day1-14_Introduction/Day9.playground/Contents.swift
+++ day1-14_Introduction/Day9.playground/Contents.swift
@@ -81,3 +81,57 @@ let uppercaseFriends = friends.map { $0.uppercased() }
 print (uppercaseFriends)
 
 //
+
+func helloUser() {
+    print("Hello!")
+}
+
+helloUser()
+var helloCopy: () -> Void = helloUser
+helloCopy()
+
+ 
+func makeArray(size: Int, using generator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+     
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+     
+    return numbers
+}
+
+let rolls = makeArray(size: 20) {
+    Int.random(in: 1...20)
+}
+
+print(rolls)
+
+func generateNumber() -> Int {
+    Int.random(in: 1...20)
+}
+
+let newRolls = makeArray(size: 20, using: generateNumber)
+print(newRolls)
+
+
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+doImportantWork {
+    print("This is the first work")
+} second: {
+    print("This is the second work")
+} third : {
+    print("This is the third work")
+}
+
+//
