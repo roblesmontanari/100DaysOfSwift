136a7b9f0095dd1e389d161ef5e97ed1e031bf6b
Robles Montanari <robles.png@gmail.com>
2022-04-15 22:11:55 -0300

    Checkpoint 6 commit

---
+++ day1-14_Introduction/Checkpoint6.playground/Contents.swift
@@ -0,0 +1,34 @@
+/* CHECKPOINT 6:
+ 
+    Create a struct to store information about a car.
+     Include:
+         -> Model
+         -> Number of seats
+         -> Current gear
+     . Add a method to change gears up or down
+     . Have a think about variables and access control
+     . Don't allow invalid gears - 1...10 seems a fair maximum range
+ */
+
+import Cocoa
+
+struct Car {
+    let model = String.self
+    let seats = Int.self
+    let maxGears: Int
+    private(set) var gear = 1
+    
+    mutating func changeGear(difference: Int) {
+       let newGear = gear + difference
+
+        if newGear >= 1 && newGear <= 10 {
+            gear = newGear
+        }
+    }
+}
+
+var car = Car(model: "McLaren 720s", seats: 5, maxGears: 9)
+print(car)
+
+car.changeGear(difference: 7)
+print(car.gear)
