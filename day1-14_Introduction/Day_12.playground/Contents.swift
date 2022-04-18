b2f1a3e882314051c58de5e92cb80ca85dec5f0b
Robles Montanari <robles.png@gmail.com>
2022-04-18 16:56:29 -0300

    Day 12 commit

--- day1-14_Introduction/Day_12.playground/Contents.swift
+++ day1-14_Introduction/Day_12.playground/Contents.swift
@@ -53,3 +53,25 @@ joseph.work()
 
 let john = Developer(hours: 8)
 john.printSummary()
+
+
+// how to add initializers for classes
+
+class Vehicle {
+    let isElectric: Bool
+    
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+
+class Car: Vehicle {
+    let isConvertible: Bool
+    
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
+    }
+}
+
+let teslaX = Car(isElectric: true, isConvertible: false)
