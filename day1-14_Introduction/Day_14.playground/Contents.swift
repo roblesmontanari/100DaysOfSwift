335a73759e52fe16cd46d10eb18c99bd260685c0
Robles Montanari <robles.png@gmail.com>
2022-04-22 20:27:57 -0300

    Day 14 commit

--- day1-14_Introduction/Day_14.playground/Contents.swift
+++ day1-14_Introduction/Day_14.playground/Contents.swift
@@ -43,4 +43,31 @@ func printSquare(of number: Int?) {
 }
 
 
+// how to unwrap optionals with nil coalescing
+
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+let new = captains["Serenity"] ?? "N/A"
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let number0 = Int(input) ?? 0
+print(number0)
+
+
 //
