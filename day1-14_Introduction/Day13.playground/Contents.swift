4bb3f8586cbc4481507a33c4dd6e24f8fc50e70b
Robles Montanari <robles.png@gmail.com>
2022-04-22 18:08:46 -0300

    Day 13 commit

--- day1-14_Introduction/Day13.playground/Contents.swift
+++ day1-14_Introduction/Day13.playground/Contents.swift
@@ -126,4 +126,36 @@ extension Book {
 let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
 
 
+// how to create and use protocol extensions
+
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+protocol Person {
+    var name: String { get }
+    func sayHello ()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)!")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
+
 //
