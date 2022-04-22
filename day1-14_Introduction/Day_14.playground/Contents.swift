94c9ba94a1085f48e56d7e54e5b49547a0081f5e
Robles Montanari <robles.png@gmail.com>
2022-04-22 20:20:24 -0300

    Day 14 commit

--- day1-14_Introduction/Day_14.playground/Contents.swift
+++ day1-14_Introduction/Day_14.playground/Contents.swift
@@ -31,4 +31,16 @@ if let unwrappedNumber = number {
 }
 
 
+// how to unwrap optionals with guard
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+    
+    print("\(number) x \(number) is \(number * number)")
+}
+
+
 //
