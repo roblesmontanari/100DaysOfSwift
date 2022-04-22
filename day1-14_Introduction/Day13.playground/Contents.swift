d7dcf1d105804128a36dc26d8f96799d6d911e94
Robles Montanari <robles.png@gmail.com>
2022-04-22 17:52:37 -0300

    Day 13 commit

--- day1-14_Introduction/Day13.playground/Contents.swift
+++ day1-14_Introduction/Day13.playground/Contents.swift
@@ -80,4 +80,50 @@ func getRandomBool() -> some Equatable {
 print(getRandomNumber() == getRandomNumber())
 
 
+// how to create and use extensions
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+var quote = "  The truth is rarely pure and never simple  "
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+quote.trim()
+
+let lyrics = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+
+print(lyrics.lines.count)
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
+
+
 //
